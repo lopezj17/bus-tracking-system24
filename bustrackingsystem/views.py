3de52ce1a640f8e926b54f90156385cb3cbc9c63
lopezj17 <joselopez17@my.untdallas.edu>
2024-06-24 23:08:44 -0500

    File Structure Update
    Moved the files to the correct hierarchy

---
+++ bustrackingsystem/views.py
@@ -0,0 +1,18 @@
+#from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_page(request):
+    #return HttpResponse('Welcome to the Bus Tracking System!')+
+    return render(request, 'home_page.html')
+
+def login_page(request):
+    #return HttpResponse('Welcome to the Bus Tracking System!')+
+    return render(request, 'login_page.html')
+
+def sign_up_page(request):
+    #return HttpResponse('User registration page.')
+    return render(request, 'signup_page.html')    
+
+def main_page(request):
+    #return HttpResponse('Main map and bus page.')
+    return render(request, 'main_page.html')
